34049a579e60dd75a3f02f88fa6ca6ef2d46e013
odulaja philip <odulajaphilip@gmail.com>
2022-10-08 18:57:13 +0100

    Created fist dag task chek file

--- dags/first_workflow.py
+++ dags/first_workflow.py
@@ -8,6 +8,7 @@ created on saturday 8th of october 2022
 """
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
+from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 
@@ -16,4 +17,12 @@ default_args = {"owner":"airflow", "start_date": datetime(2022, 10, 8)}
 with DAG(dag_id='workflow', 
          default_args=default_args,
          schedule_interval='@daily',
-        ) as :
+        ) as dag:
+        
+    #  task 1: check if file exist
+    check_file = BashOperator(
+        task_id="check_file",
+        bash_command="philipodulaja ~/ip_files/or.csv",
+        retries=2,
+        retry_delay= timedelta(seconds=15)
+    )
